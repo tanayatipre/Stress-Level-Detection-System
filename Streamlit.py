81966b1c2fd72aa79d908f617ea67c585bc0803b
Tanaya Tipre <112678961+tanayatipre@users.noreply.github.com>
2024-04-23 19:13:47 +0530

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -1,103 +1,86 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 16,
-   "id": "6deac1fe",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "import numpy as np\n",
-    "import pickle\n",
-    "import streamlit as st"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": 17,
-   "id": "fe50cdcc",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stderr",
-     "output_type": "stream",
-     "text": [
-      "2024-04-23 12:07:05.836 \n",
-      "  \u001b[33m\u001b[1mWarning:\u001b[0m to view this Streamlit app on a browser, run it with the following\n",
-      "  command:\n",
-      "\n",
-      "    streamlit run C:\\Users\\Tanaya\\anaconda3\\lib\\site-packages\\ipykernel_launcher.py [ARGUMENTS]\n"
-     ]
+import streamlit as st
+import joblib
+
+
+st.markdown(
+    """
+    <style>
+    .main {
+        background-color: #000033;
+        color: white;
     }
-   ],
-   "source": [
-    "import numpy as np\n",
-    "import pickle\n",
-    "import streamlit as st\n",
-    "\n",
-    "pickle_in = open(\"random_forest.pkl\", \"rb\")\n",
-    "random_forest = pickle.load(pickle_in)\n",
-    "\n",
-    "def welcome():\n",
-    "    return \"Welcome All\"\n",
-    "\n",
-    "def predict_note_authentication(Gender, Age, Occupation, Sleep_Duration, BMI_Category, Heart_Rate, Daily_Steps, Systolic_BP):\n",
-    "    # Concatenate the input variables into a list or array\n",
-    "    features = [[Gender, Age, Occupation, Sleep_Duration, BMI_Category, Heart_Rate, Daily_Steps, Systolic_BP]]\n",
-    "    # Make prediction\n",
-    "    prediction = random_forest.predict(features)\n",
-    "    return prediction\n",
-    "\n",
-    "def main():\n",
-    "    st.title(\"Stress Level Detection\")\n",
-    "    html_temp = \"\"\"\n",
-    "    <div style=\"background-color:tomato;padding:10px\">\n",
-    "    <h2 style=\"color:white;text-align:center;\">Streamlit Stress Level Detection ML App </h2>\n",
-    "    </div>\n",
-    "    \"\"\"\n",
-    "    st.markdown(html_temp, unsafe_allow_html=True)\n",
-    "    Gender = st.text_input(\"Gender\", \"Type Here\")\n",
-    "    Age = st.text_input(\"Age\", \"Type Here\")\n",
-    "    Occupation = st.text_input(\"Occupation\", \"Type Here\")\n",
-    "    Sleep_Duration = st.text_input(\"Sleep Duration\", \"Type Here\")\n",
-    "    BMI_Category = st.text_input(\"BMI Category\", \"Type Here\")\n",
-    "    Heart_Rate = st.text_input(\"Heart Rate\", \"Type Here\")\n",
-    "    Daily_Steps = st.text_input(\"Daily Steps\", \"Type Here\")\n",
-    "    Systolic_BP = st.text_input(\"Systolic BP\", \"Type Here\") \n",
-    "    result = \"\"\n",
-    "    if st.button(\"Detect\"):\n",
-    "        # Pass the input values to the prediction function\n",
-    "        result = predict_note_authentication(Gender, Age, Occupation, Sleep_Duration, BMI_Category, Heart_Rate, Daily_Steps, Systolic_BP)\n",
-    "        st.success('The output is {}'.format(result))\n",
-    "\n",
-    "    if st.button(\"About\"):\n",
-    "        st.text(\"Let's Learn\")\n",
-    "        st.text(\"Built with Streamlit\")\n",
-    "\n",
-    "if __name__ == '__main__':\n",
-    "    main()\n"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.9.12"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+
+    .stLabel, .stTextInput>div>div>div>input, .stTextInput>div>div>label, .st-emotion-cache-ue6h4q {
+        color: white !important;
+    }
+    
+
+    .st-emotion-cache-1umgz6k{
+        background-color: rgb(49 49 194);
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+#pickle_in = open("random_forest.pkl","rb")
+random_forest = joblib.load('random_forest1.pkl')
+
+# st.title("Stress Level Detection")
+html_temp = """
+    <div>
+    <h2 style="color:white;text-align:center;"><img src ="https://cdn-icons-png.flaticon.com/512/2755/2755341.png" width="50" height="50" style="margin-right: 10px;">Stress Level Detection App </h2>
+    </div>
+    """
+st.markdown(html_temp,unsafe_allow_html=True)
+gender_options = ['Select Gender','Male', 'Female']
+Gender = st.selectbox('Gender', gender_options, index=0)
+
+Age = st.text_input("Age", placeholder="Type Here", key="age")
+
+occupation_options = ['Select Occupation', 'Scientist', 'Doctor', 'Accountant', 'Teacher', 'Manager', 'Engineer', 'Sales Representative', 'Lawyer', 'Salesperson', 'Software Engineer', 'Nurse']
+Occupation = st.selectbox('Occupation', occupation_options, index=0)
+
+
+Sleep_Duration = st.text_input("Sleep Duration", placeholder="Type Here")
+
+bmi_category_options = ['Select BMI Category', 'Underweight', 'Normal' ,'Overweight']
+BMI_Category = st.selectbox('BMI Category', bmi_category_options, index=0)
+
+Heart_Rate = st.text_input("Heart Rate", placeholder="Type Here")
+Daily_Steps = st.text_input("Daliy Steps", placeholder="Type Here")
+Systolic_BP = st.text_input("Systolic BP", placeholder="Type Here")
+
+# Preprocessing the user inputs
+Gender = 1 if Gender == 'Male' else 0
+Occupation = occupation_options.index(Occupation) - 1
+BMI_Category = bmi_category_options.index(BMI_Category) - 1
+
+
+user_inputs = [[
+    Gender,
+    int(Age) if Age else 0,  # Use 0 if Age is an empty string
+    Occupation,
+    float(Sleep_Duration) if Sleep_Duration else 0,  # Use 0 if Sleep_Duration is an empty string
+    BMI_Category,
+    int(Heart_Rate) if Heart_Rate else 0,  # Use 0 if Heart_Rate is an empty string
+    int(Daily_Steps) if Daily_Steps else 0,  # Use 0 if Daily_Steps is an empty string
+    int(Systolic_BP) if Systolic_BP else 0  # Use 0 if Systolic_BP is an empty string
+]]
+# if st.button("Predict"):
+#     result = random_forest.predict(user_inputs)
+
+#     st.write(f"Predicted stress level: {result[0]}")
+
+col1, col2, col3 = st.columns([1, 1, 1])
+with col2:
+    if st.button("Predict", key="predict_button", use_container_width=True):
+        result = random_forest.predict(user_inputs)
+        # Center-align and bold the predicted stress level
+        html_result = f"""
+        <div style="text-align:center;">
+            <p style="font-weight:bold;">Predicted stress level: {result[0]}</p>
+        </div>
+        """
+        st.markdown(html_result, unsafe_allow_html=True)
+    
